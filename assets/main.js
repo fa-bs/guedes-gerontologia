891782f48b15a89bbfa4119b62725ba1459cfb7c
fa-bs <0.fabiobs@gmail.com>
2024-03-19 22:36:16 -0300

    update javascript

--- assets/main.js
+++ assets/main.js
@@ -1,18 +1,3 @@
-/* var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-} */
-
 const accodions = document.querySelectorAll(".accordion");
 accodions.forEach(acc => {
     acc.addEventListener("click", () => {
